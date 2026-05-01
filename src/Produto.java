2cb46a430fb80fee50463727afb670c2cb5e79e2
HeloisaSVasconcelos <souzaheloisa277@gmail.com>
2026-04-30 21:12:43 -0400

    Exercicio 2

--- src/Produto.java
+++ src/Produto.java
@@ -2,9 +2,14 @@ class Produto {
     String nome;
     double preco;
 
-    // Construtor com os dois atributos
     public Produto(String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
+    }
+
+    // Adicionando o @Override toString()
+    @Override
+    public String toString() {
+        return "Produto{nome='" + nome + "', preco=" + preco + "}";
     }
 }
